2cfc65ca0004d73407e6a4c544c45cc3f3fd307c
tditada <tditada@gmail.com>
2014-04-11 02:18:08 -0300

    Commenting functions right

--- include/client.h
+++ include/client.h
@@ -13,7 +13,7 @@ void add_product(char * prodname, int quantity);
 // removes a product from the stock altogether - the opposite of add_product
 void remove_product(char * prodname);
 
-// Updates product to new quantity with name == prodname
+// Adds 'quantity' to product with name == prodname
 void deposit_product(char * prodname, int quantity);
 // removes 'quantity' products with name == prodname - the opposite of deposit_product
 void take_product(char * prodname, int quantity);
