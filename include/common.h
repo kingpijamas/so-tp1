d6b2d2de0ef6520925cc449ca3ea60ade9f75096
tditada <tditada@gmail.com>
2014-04-11 01:36:34 -0300

    Common.h

--- include/common.h
+++ include/common.h
@@ -1,11 +1,16 @@
 #ifndef _COMMON_
 #define _COMMON_
 
+#include "model.h"
+
 #define OK 0
 #define ERROR -1
+#define MAX_NAME 1000
 
 typedef enum { false,true } boolean;
 
 typedef char * string;
+
+void product_init(Product * product);
 
 #endif
